3f4e4a94b725dca03643092f7547628fdd68b61d
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2024-12-06 16:19:10 -0800

    update shadcn deps

--- src/lib/components/ui/table/table-footer.svelte
+++ src/lib/components/ui/table/table-footer.svelte
@@ -8,6 +8,6 @@
 	export { className as class };
 </script>
 
-<tfoot class={cn('bg-primary font-medium text-primary-foreground', className)} {...$$restProps}>
+<tfoot class={cn('bg-muted/50 font-medium text-primary-foreground', className)} {...$$restProps}>
 	<slot />
 </tfoot>
